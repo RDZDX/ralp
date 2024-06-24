febfcc706bb6b3b564da3ad3a681ba862433373e
Audrius Puplinskas <audrius@ipk.lt>
2024-06-24 18:43:38 +0300

    Update ralp.c

--- ralp.c
+++ ralp.c
@@ -9,28 +9,7 @@ void vm_main(void) {
 void run(void) {
 
     VMWCHAR f_wname[100];
-//-------------Nokia 225 single sim:
     VMCHAR MyApp[100] = "\\271123656.rom"; // Facebook messanger
-    //VMCHAR MyApp[100] = "\\271139084.rom"; // Facebook
-    //VMCHAR MyApp[100] = "\\271305228.rom"; // Twitter
-    //VMCHAR MyApp[100] = "\\271543384.rom"; // Opera
-    //VMCHAR MyApp[100] = "\\273716508.rom"; // Games
-    //VMCHAR MyApp[100] = "\\272222672.rom"; // Asphalt 6 adrenaline
-    //VMCHAR MyApp[100] = "\\272643436.rom"; // Block breaker unlimited
-    //VMCHAR MyApp[100] = "\\272966676.rom"; // Modern combat 2 black pegasus
-    //VMCHAR MyApp[100] = "\\273336536.rom"; // Real football 2015
-    //VMCHAR MyApp[100] = "\\274013860.rom"; // The amazing spider-man
-//-------------Nokia 225 double sim:
-    //VMCHAR MyApp[100] = "\\271307840.rom"; // Facebook messanger
-    //VMCHAR MyApp[100] = "\\271323268.rom"; // Facebook
-    //VMCHAR MyApp[100] = "\\271489412.rom"; // Twitter
-    //VMCHAR MyApp[100] = "\\271727568.rom"; // Opera
-    //VMCHAR MyApp[100] = "\\273900692.rom"; // Games
-    //VMCHAR MyApp[100] = "\\272827620.rom"; // Block breaker unlimited
-    //VMCHAR MyApp[100] = "\\273150860.rom"; // Modern combat 2 black pegasus
-    //VMCHAR MyApp[100] = "\\272406856.rom"; // Asphalt 6 adrenaline
-    //VMCHAR MyApp[100] = "\\271498044.rom"; // The amazing spider-man
-    //VMCHAR MyApp[100] = "\\273520720.rom"; // Real football 2015
 
     vm_ascii_to_ucs2(f_wname, (strlen(MyApp) + 1) * 2, MyApp);
     vm_start_app(f_wname, 0, 0);
@@ -42,3 +21,42 @@ void timer(int a) {
      vm_delete_timer_ex(a);
      vm_exit_app();
 }
+
+/*---------------------------------------
+Nokia 225 single sim:
+"\\271123656.rom" - Facebook messanger +
+"\\271139084.rom" - Facebook +
+"\\271305228.rom" - Twitter +
+"\\271543384.rom" - Opera +
+"\\273716508.rom" - Games +
+"\\272222672.rom" - Asphalt 6 adrenaline +
+"\\272643436.rom" - Block breaker unlimited +
+"\\272966676.rom" - Modern combat 2 black pegasus +
+"\\273336536.rom" - Real football 2015 +
+"\\274013860.rom" - The amazing spider-man +
+
+Nokia 225 double sim:
+"\\271307840.rom" - Facebook messanger
+"\\271323268.rom" - Facebook
+"\\271489412.rom" - Twitter
+"\\271727568.rom" - Opera
+"\\273900692.rom" - Games
+"\\272827620.rom" - Block breaker unlimited
+"\\273150860.rom" - Modern combat 2 black pegasus
+"\\272406856.rom" - Asphalt 6 adrenaline
+"\\271498044.rom" - The amazing spider-man
+"\\273520720.rom" - Real football 2015
+
+Nokia 225 single sim old:
+"//271297640.rom" - Yahoo messanger
+"//273990116.rom" - Games
+"//274351780.rom" - Facebook
+"//271131496.rom" - Facebook messanger
+"//271116068.rom" - Nokia xpress
+"//271529340.rom" - Twitter
+"//272242400.rom" - Asphalt 6 adrenaline
+"//272620288.rom" - Block breaker unlimited
+"//273033712.rom" - Avengers
+"//273356952.rom" - Assasins creeed 3
+"//273703208.rom" - Real football 2012
+----------------------------------------*/
